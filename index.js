3f7b70c02b37340cb7da0f446fddc54cb4b1dcff
Mark Ogunade <olamideogunade1205@gmail.com>
2022-06-23 00:20:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+alert('Name:Mark Ogunade Height:5ft8 Country:Nigeria');
+
+document.write('<h1>NAME: Mark Ogunade.</h1> <br>')
+
+document.write('<h1>HEIGHT: 5ft8 </h1> <br>')
+
+document.write('<h1>COUNTRY: Nigeria. </h1>')
